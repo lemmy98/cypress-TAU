897ccca50ec1dc868ab9083af808d6087032c8d6
lemmy98 <lemikanemmanuel@gmail.com>
2025-03-12 10:07:59 +0100

    Removed ignored files

--- node_modules/@applitools/eyes-cypress/index.js
+++
@@ -1,3 +0,0 @@
-const expose = require('./dist/expose')
-module.exports = expose.default
-Object.defineProperty(module.exports, 'default', {value: expose.default})
--- node_modules/@applitools/eyes-cypress/src/setup/isPluginDefinedESM.js
+++
@@ -1,5 +0,0 @@
-function isPluginDefinedTypeScript(content) {
-  return !!content.match(/from\s*['"]@applitools\/eyes-cypress['"]\s*/)
-}
-
-module.exports = isPluginDefinedTypeScript
